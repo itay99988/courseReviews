2c6d1b49caab3106d4bd588411bac27b79fabfe6
Itay Cohen <itay99988@gmail.com>
2017-10-25 11:47:27 +0300

    change a file path

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var Course = require('../services/mongoService');
+var Course = require('../services/mongoService.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
